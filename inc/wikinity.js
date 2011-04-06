002913115d97c8541f4fc647c6311d18962b25e7
suurjaak@gmail.com <suurjaak@gmail.com@dedead77-9d2b-8447-517c-ac3fc5d86d27>
2011-04-06 10:02:15 +0000

    Rearranged source structure, deleting deprecated parts (like backend code) and renamed some files.
    git-svn-id: http://wikinity.googlecode.com/svn/trunk@47 dedead77-9d2b-8447-517c-ac3fc5d86d27

---
+++ inc/wikinity.js
@@ -0,0 +1,353 @@
+// All page logic. Expects the jQuery and Arbor scripts to be included.
+//
+// wikinity_backend or wikinity_wiki must also be included.
+//
+// To avoid confusion between the jQuery and Arbor and Wikinity nodes:
+// - node is a Wikinity node
+// - element is a jQuery node
+// - vertice is an Arbor node
+//
+// @author    Erki Suurjaak
+// @created   02.04.2011
+// @modified  07.04.2011
+
+// jQuery replaces ? with the created callback function name, this allows for
+// cross-site requests.
+const DEFAULT_LINKS_LIMIT = 5;
+const DEFAULT_NEIGHBORHOOD_SIZE = 1;
+const RESIZE_STEP_WIDTH = 10;
+const RESIZE_STEP_HEIGHT = 20;
+
+var sys = null; // Arbor ParticleSystem instance
+var gfx = null; // Arbor Graphics instance
+var nodes = {}; // {title: node object, }
+var focused_node = null; // Currently focused node
+
+var limit = DEFAULT_LINKS_LIMIT;
+var images_enabled = true;
+var autoclear_results = true;
+var neighborhood_size = DEFAULT_NEIGHBORHOOD_SIZE;
+var autohide_contents = false;
+
+
+/**
+ * Creates a new graph node and adds it to screen. If a node by this title
+ * already exists, updates its contents.
+ *
+ * @param   data             data object, with title, snippet etc
+ * @param   referring_title  title of the node from where the add originates, if any
+ * @return                   the node object
+ */
+function add_node(data, referring_title) {
+  var node = nodes[data.title];
+  if (!node) {
+    node = {links_queried: false, complete: false, autocollapsed: autohide_contents, usercollapsed: false, collapsed: autohide_contents };
+    node.id = new Date().getTime();
+    node.data = data;
+    node.title = data.title;
+    node.element = create_graph_element(node);
+    if (data.snippet) {
+      node.complete = true;
+    }
+    nodes[node.title] = node;
+    node.vertice = sys.addNode(node.id, {"title": node.title, "node": node});
+    if (referring_title) {
+      sys.addEdge(node.id, nodes[referring_title].id);
+    }
+  } else if (!node.complete) {
+    update_graph_element(node, data);
+    node.data = data;
+    node.complete = true;
+  }
+  return node;
+}
+
+
+function remove_node(node) {
+  delete nodes[node.title];
+  sys.pruneNode(node.id);
+  node.element.remove();
+}
+
+
+function clear_results() {
+  $("#results").empty();
+  for (var title in nodes) {
+    nodes[title].element.remove();
+  }
+  nodes = {};
+  if (sys) sys.prune(function(node, from, to) {
+    // dummy nodes don't have the node member
+    if (node.data.node) return true;
+  });
+  if (gfx) gfx.clear();
+}
+
+
+function open_settings() {
+  $("#settings").slideToggle("fast");
+}
+
+
+function update_settings() {
+  limit = parseInt($("#setting_link_limit").val());
+  if (NaN == limit) {
+    limit = DEFAULT_LINKS_LIMIT;
+  }
+  neighborhood_size = parseInt($("#setting_neighborhood_size").val());
+  if (NaN == neighborhood_size) {
+    neighborhood_size = DEFAULT_NEIGHBORHOOD_SIZE;
+  }
+  images_enabled = $("#setting_images_enabled").is(":checked");
+  autoclear_results = $("#setting_autoclear_results").is(":checked");
+  autohide_contents = $("#setting_autohide_contents").is(":checked");
+}
+
+
+/**
+ * Returns the node for the specified jQuery div element.
+ */
+function find_node(wid) {
+  for (var title in nodes) {
+    if (nodes[title].id == wid)
+      return nodes[title];
+  }
+}
+
+
+/**
+ * Makes the node bigger or smaller, and updates its display to show
+ * more text if needed.
+ */
+function on_node_mousewheel(event, delta, node) {
+  if (node) {
+    var element = node.element;
+  } else {
+    var element = $(this);
+    node = find_node(element.attr("wid"));
+  }
+  if (node.complete) {
+    var content = element.find("div.content");
+    var heading = element.find("h1:first");
+    var text = content.find("div:first");
+
+    if (text) {
+      var current_width = element.width();
+      var current_height = element.height();
+      if (!node.initial_full_width && !node.collapsed) {
+        // Remember first dimensions, to animate uncollapsing
+        node.initial_full_width = current_width;
+        node.initial_full_height = current_height;
+      }
+      var new_width = current_width + RESIZE_STEP_WIDTH * delta;
+      var new_height = current_height + RESIZE_STEP_HEIGHT * delta;
+
+      var new_inner_width = new_width - element.outerWidth() + current_width;
+      var new_inner_height = new_height - element.outerHeight() + current_height;
+      if (node.collapsed && delta > 0) {
+        // Enlarge node to initial default
+        if (node.initial_full_width) {
+          element.css("max-width", "");
+          element[0].style["max-width"] = ""; 
+          element.animate({"width": node.initial_full_width, "height": node.initial_full_height}, 200, function() { content.css("display", "block"); } );
+        } else  {
+            content.css("display", "block");
+        }
+        //element.css({"height": "auto", "width": 200});
+        node.collapsed = false;
+      } else {
+        if (delta > 0 || heading.height() < new_inner_height && heading.width() < new_inner_width) {
+          // Skip enlarging if we already see everything
+          if (!node.resized || !(delta > 0 && element.outerHeight() > content.position().top + content.outerHeight())) {
+            if (delta > 0 && !node.fullsnippet_displayed) {
+              // Put the full content instead of the shorter one.
+              text.empty().append(node.snippet_element);
+              node.fullsnippet_displayed = true;
+            }
+            element.css({"width": new_width, "height": new_height});
+            node.resized = true;
+          }
+        } else if (!node.collapsed) {
+          // Sides getting too small - collapse node
+          content.css("display", "none");
+          // Put back the shorter content.
+          text.empty().append(node.shorter_snippet_element);
+          element.css({"height": "auto", "width": "auto", "max-width": "200px"});
+          node.collapsed = true;
+          node.fullsnippet_displayed = false;
+          node.resized = false;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+
+var Renderer = function(elt){
+  var canvas_element = $(elt);
+  var canvas_dom = canvas_element.get(0);
+  var ctx = canvas_dom.getContext("2d");
+  gfx = arbor.Graphics(canvas_dom);
+
+  var that = {
+    init:function(pSystem){
+      sys = pSystem;
+      sys.screen({size:{width:canvas_element.width(), height:canvas_element.height()},
+                  padding:[36,60,36,60]});
+
+    },
+    redraw:function(){
+      gfx.clear()
+      sys.eachEdge(function(edge, pt1, pt2) {
+        // edge: {source:Node, target:Node, length:#, data:{}}
+        // pt1:  {x:#, y:#}  source position in screen coords
+        // pt2:  {x:#, y:#}  target position in screen coords
+        // draw a line from pt1 to pt2
+        if (edge.source.data.node && edge.target.data.node && edge.source != edge.target) { // To skip dummy elements
+          ctx.strokeStyle = "rgba(0,0,0, .333)";
+          ctx.lineWidth = (edge.source.data.node == focused_node || edge.target.data.node == focused_node) ? 2 : 1;
+          ctx.beginPath();
+          ctx.moveTo(pt1.x, pt1.y);
+          ctx.lineTo(pt2.x, pt2.y);
+          ctx.stroke();
+        }
+      })
+      sys.eachNode(function(node, pt) {
+        if (node.data.node) {
+          node.data.node.element.css("display", "block"); // Initially was set to none
+          var element = node.data.node.element;
+          var heading = node.data.node.element.find("h1");
+          if (!heading) heading = node.data.node.element.find("h2");
+          var x = canvas_element.position().left + pt.x - element.outerWidth() / 2;
+          var y = heading ? (canvas_element.position().top + pt.y - parseInt(heading.css("font-size")) / 2) : canvas_element.position().top + pt.y;
+/*
+          Was trying out how to better manage not letting divs too much over the edge,
+          as the div has dimension, but points don't. Doesn't work very well, is slow
+          and buggy. Especially - slow.
+          var x = element.position().left;
+          var y = element.position().top;
+          var new_x = canvas_element.position().left + pt.x - element.outerWidth() / 2;
+          var new_y = heading ? (canvas_element.position().top + pt.y - parseInt(heading.css("font-size")) / 2) : canvas_element.position().top + pt.y;
+          if (new_x > 0 && new_x + element.outerWidth() < canvas_element.position().left + canvas_element.outerWidth()) {
+            x = new_x;
+          } else {
+            x = (new_x < 0) ? canvas_element.position().left : canvas_element.position().left + canvas_element.outerWidth();
+          }
+          if (new_y > 0 && new_y + element.outerHeight() < canvas_element.position().top + canvas_element.outerHeight()) {
+            y = new_y;
+          } else {
+            y = (new_y < 0) ? canvas_element.position().top : canvas_element.position().top + canvas_element.outerHeight();
+          }
+*/
+          //@todo needs some tweaking
+          //x = Math.max(0, Math.min(x, canvas_dom.offsetWidth));
+          //y = Math.max(0, Math.min(y, canvas_dom.offsetHeight));
+          element.css({"left": x, "top": y});
+          //node.data.node.element.animate({"top": y, "left": x}, 100);
+        }
+      })
+    },
+  }
+  
+  return that;
+}
+
+
+function parse_hash() {
+
+    var hashParams = {};
+    var e,
+        a = /\+/g,  // Regex for replacing addition symbol with a space
+        r = /([^&;=]+)=?([^&;]*)/g,
+        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+        q = window.location.hash.substring(1);
+
+    while (e = r.exec(q))
+       hashParams[d(e[1])] = d(e[2]);
+
+    return hashParams;
+}
+
+
+$(document).ready(function(){
+
+  $("#search_form").submit(
+    function(){
+      term = $.trim($("#search_term").val());
+      if (term) { 
+        if (autoclear_results) clear_results();
+        get_page(term, neighborhood_size);
+      }
+      return false;
+    }
+  );
+
+  $("#setting_link_limit").val(DEFAULT_LINKS_LIMIT);
+
+  $("#setting_neighborhood_size").val(DEFAULT_NEIGHBORHOOD_SIZE);
+
+  $("#clear_button").click(clear_results);
+
+  $("#settings_button").click(open_settings);
+
+  $("#setting_images_enabled").click(function() { images_enabled = $("#setting_images_enabled").is(":checked"); });
+
+  $("#setting_autoclear_results").click(function() { autoclear_results = $("#setting_autoclear_results").is(":checked"); });
+
+  $("#setting_autohide_contents").click(function() { autohide_contents = $("#setting_autohide_contents").is(":checked"); for (var title in nodes) { nodes[title].autocollapsed = autohide_contents; if (!nodes[title].usercollapsed) on_node_mousewheel(null, autohide_contents ? -100 : 1, nodes[title]); } });
+
+  $("#setting_link_limit").change(function() { limit = parseInt($("#setting_link_limit").val()); if (NaN == limit) limit = DEFAULT_LINKS_LIMIT; });
+
+  $("#setting_neighborhood_size").change(function() { neighborhood_size = parseInt($("#setting_neighborhood_size").val()); if (NaN == neighborhood_size) neighborhood_size = DEFAULT_LINKS_LIMIT; });
+
+
+  $(window).load(function () { 
+    $('#search_term').focus(); 
+
+    // Get parameters from hash string, like #images_enabled=1&link_limit=1&neighborhood_size=10&term=SEARCHTERM
+    var hash_params = parse_hash();
+    for (var name in hash_params) {
+      if ("term" == name) {
+        $("#search_term").val(hash_params[name]);
+      } else if ("limit" == name) {
+        $("#setting_link_limit").val(hash_params[name]);
+      } else if ("depth" == name) {
+        $("#setting_neighborhood_size").val(hash_params[name]);
+      } else if ("images" == name) {
+        $("#setting_images_enabled").attr("checked", "0" != hash_params[name]);
+      } else if ("autohide" == name) {
+        $("#setting_autohide_contents").attr("checked", "0" != hash_params[name]);
+      } else if ("autoclear" == name) {
+        $("#setting_autoclear_results").attr("checked", "0" != hash_params[name]);
+      }
+    }
+    update_settings();
+    if (hash_params["term"]) {
+      $("#search_form").submit();
+    }
+  });
+
+  sys = arbor.ParticleSystem()
+  // @todo twiddle with these. From Arbor doc:
+  // NAME     DEFAULT  INFO
+  // repulsion  1,000  the force repelling nodes from each other
+  // stiffness    600  the rigidity of the edges
+  // friction     0.5  the amount of damping in the system
+  // gravity    false  an additional force attracting nodes to the origin
+  // fps           55  frames per second
+  // dt          0.02  timestep to use for stepping the simulation
+  // precision    0.6  accuracy vs. speed in force calculations
+  //                   (zero is fast but jittery, one is smooth but cpu-intensive)
+  //sys.parameters({friction:0.5, stiffness:100, repulsion:1000, gravity: true, fps: 60, dt: 0.08, precision: 1.0});
+  sys.parameters({stiffness:900, repulsion:1000, gravity: true});
+  sys.renderer = Renderer("#canvas")
+
+  // Perhaps I'm an ass, but it seems that Arbor cannot handle just one node in the
+  // graph. At all. If there is first only one element, and another is added later,
+  // Arbor stops updating. <shrug> So a temporary workaround: use invisible dummies.
+  sys.addNode("dummy1", {"title": "dummy1", "fixed": true}); // Reference says that on fixed=true the node is
+  sys.addNode("dummy2", {"title": "dummy2", "fixed": true}); // unaffected by other nodes, but doesn't seem to work.
+//  sys.addEdge("dummy1", "dummy2");
+
+})
---
+++ inc/wikinity_wiki.js
@@ -0,0 +1,331 @@
+// Functions for querying the Wikipedia directly with in-browser JSON
+// requests, and doing wiki-specific actions.
+//
+// To be included together with wikinity.js.
+//
+// @author    Erki Suurjaak
+// @created   03.04.2011
+// @modified  06.04.2011
+
+// jQuery replaces ? with the created callback function name, this allows for
+// cross-site requests.
+const WIKI_API_URL = "http://en.wikipedia.org/w/api.php?callback=?";
+// All the other wikipedia namespaces besides the main one (0). Articles
+// in the namespaces have the namespace name as prefix (e.g. WP:Logo).
+// http://en.wikipedia.org/wiki/Wikipedia_namespaces
+const OTHER_NAMESPACES = ["Wikipedia", "WP", "Project", "WT", "Project talk",
+                          "Image", "File", "Image talk", "Portal", "User",
+                          "MediaWiki", "Template", "Category", "Book", "Help"];
+const MAX_IMG_WIDTH = 60;
+const MAX_IMG_HEIGHT = 80;
+
+
+
+/**
+ * Creates and returns the jQuery div element for the wiki page.
+ *
+ * @param   node  the local graph node object
+ * @return        the jQuery div object containing the node HTML
+ */
+function create_graph_element(node) {
+  var element = $("<div />");
+  element.attr("wid", node.id); // Attach wikinity id to the element
+  $("<a />").attr({"class": "wiki", "title": "open wiki", "href": "http://en.wikipedia.org/wiki/"+node.data.title}).text("w").appendTo(element);
+  $("<a />").attr({"class": "close", "title": "close"}).text("x").click(function() { remove_node(node); return false; }).appendTo(element);
+  $("<a />").attr({"class": "toggle", "title": "toggle visibility"}).text("*").click(function() { node.usercollapsed = !node.collapsed; on_node_mousewheel(null, node.collapsed ? 1 : -100, node); return false; }).appendTo(element);
+  element.css("background", "#F6F6F6")
+  var heading = $(node.data.snippet ? "<h1 />" : "<h2 />").html(node.data.title).appendTo(element);
+  element.hover(function() { focused_node = node; if (node.collapsed && !node.usercollapsed) on_node_mousewheel(null, 1, node); }, function() { focused_node = null; if (autohide_contents && node.autocollapsed) on_node_mousewheel(null, -100, node); });
+  element.mousewheel(function(event, delta) { on_node_mousewheel(event, delta, node); node.usercollapsed = !node.usercollapsed && node.collapsed; node.autocollapsed = node.autocollapsed && node.collapsed && !node.usercollapsed; });
+  if (node.data.snippet) {
+    // Wrap snippet in <span> as it can contain a flat list of HTML
+    var snippet = $("<span />").html(node.data.snippet);
+    // Parse out tables and divs, those tend to not have text content
+    snippet.find("table").empty().remove(); // Emptying first could be faster
+    snippet.find("div").remove();
+    snippet.find("strong.error").remove(); // Wiki error messages
+    snippet.find("span.IPA").remove(); // phonetic alphabet media content
+    snippet.find("img").remove();
+    process_links(snippet, node);
+    var shorter_snippet = $($.trim(snippet.html().substr(0, 1000)));
+    $("<div />").attr("class", "content").css("display", autohide_contents ? "none": "block").append($("<div />").attr("class", "text").append(shorter_snippet)).appendTo(element);
+    node.snippet_element = snippet;
+    node.shorter_snippet_element = shorter_snippet;
+    heading_click_function = function() { if (!node.links_queried) node.links_queried = true; get_see_also(node.data.title, neighborhood_size); };
+  } else {
+    var heading_click_function = function() { if (!node.links_queried) node.links_queried = true; get_page(node.data.title, neighborhood_size); }
+  }
+  heading.click(heading_click_function);
+  heading.hover(function() { if (!node.links_queried) heading.css('cursor','pointer'); }, function() { heading.css('cursor','auto'); });
+  element.appendTo("#results");
+  return element;
+}
+
+
+/**
+ * Updates the graph element with the new data. Checks whether the new data is
+ * different from node's current data.
+ */
+function update_graph_element(node, data) {
+  if (data.snippet && !node.data.snippet) {
+    var element = node.element;
+    element.children().not("a").remove(); // New info arrived, empty all except close/wiki links
+    var heading = $("<h1 />").html(data.title).appendTo(element);
+    // Wrap snippet in <span> as it can contain a flat list of HTML
+    var snippet = $($("<span />").html(data.snippet));
+    // Parse out tables, those tend to not have text content
+    snippet.find("table").empty().remove(); // Emptying first should be faster
+    snippet.find("div").remove();
+    snippet.find("strong.error").remove(); // Wiki error messages
+    snippet.find("span.IPA").remove(); // phonetic alphabet media content
+    snippet.find("img").remove();
+    process_links(snippet, node);
+    var shorter_snippet = $($.trim(snippet.html().substr(0, 1000)));
+    $("<div />").attr("class", "content").css("display", autohide_contents ? "none": "block").append($("<div />").attr("class", "text").append(shorter_snippet)).appendTo(element);
+    node.snippet_element = snippet;
+    node.shorter_snippet_element = shorter_snippet;
+    heading_click_function = function() { if (!node.links_queried) node.links_queried = true; get_see_also(data.title, neighborhood_size); };
+  } else {
+    var heading = element.find("h1:first");
+    heading_click_function = function() { if (!node.links_queried) node.links_queried = true; get_page(data.title, neighborhood_size); }
+  }
+  heading.click(heading_click_function);
+  heading.hover(function() { if (!node.links_queried) heading.css('cursor','pointer'); }, function() { heading.css('cursor','auto'); });
+}
+
+
+
+/**
+ * Processes the links in the element, replacing relative hrefs with absolute
+ * ones and attaching click handlers to open links inside Wikinity.
+ */
+function process_links(element, node) {
+  element.find("a").each(function(index, a) {
+    var link = $(this);
+    link.link_clicked = false;
+    var href = link.attr("href");
+    if ("#" == href[0]) {
+      link.remove(); // On-page link, remove. @todo leave link contents
+    } else if ("/wiki/" == href.slice(0, 6)) {
+      var new_title = decodeURIComponent(href.slice(6));
+      if (!node.links) {
+        node.links = {};
+      }
+      node.links[new_title] = {};
+      link.attr("href", "http://en.wikipedia.org" + href);
+      // Have clicking the link make a local query
+      // Click handlers had problems: when swapping node content between snippet and shorter_snippet on resizing,
+      // handlers got lost. Don't know why.
+      //link.click(function() { if (!link.link_clicked) get_page(new_title, neighborhood_size, node.title); link.link_clicked = true; return false; });
+      // @todo fix this fuckery
+      link.attr("onclick", "javascript:get_page('"+new_title.replace("'", "\\'")+"', neighborhood_size, '"+node.title.replace("'", "\\'")+"'); return false;");
+    } else if ("/w/" == href.slice(0, 3)) {
+      // Probably an edit link
+      link.attr("href", "http://en.wikipedia.org" + href);
+    }
+  });
+}
+
+
+/**
+ * Gets the page with the specified title. Skips if the page is already fully
+ * retrieved.
+ *
+ * @return    true if a query was made, false otherwise
+ */
+function get_page(title, depth_to_follow, connected_page) {
+  result = true;
+  if (!nodes[title] || !nodes[title].complete)
+  $.getJSON(WIKI_API_URL,
+    {
+      "action": "parse",
+      "disablepp": "1",
+      "redirects": "1",
+      "section": "0",
+      "format": "json",
+      "page": title,
+    },
+    function(data) {
+        if (data.parse && data.parse.displaytitle) {
+          if (data.parse.displaytitle != title && nodes[title]) {
+            // Page title is different in the article from the link: remove
+            // the old node by this name, as its name can no longer be modified.
+            remove_node(nodes[title]);
+          }
+          var page = {title: $("<span />").html(data.parse.displaytitle).text(), snippet: (data.parse.text)["*"], images: []};
+          for (var i in data.parse.images) {
+            var accept = true;
+            if (".svg" == data.parse.images[i].slice(-4)) { // SVG images are probably wiki icons
+              accept = false;
+            } else if ("Ambox_content.png" == data.parse.images[i]) {
+              accept = false;
+            }
+            if (accept) {
+              page.images.push(data.parse.images[i]);
+            }
+          }
+
+          var node = add_node(page, connected_page);
+
+          depth_to_follow = typeof(depth_to_follow) != 'undefined' ? depth_to_follow : 1;
+          if (depth_to_follow) {
+            node.links_queried = true;
+            get_see_also(page.title, depth_to_follow);
+          }
+
+          if (page.images.length && images_enabled) {
+            get_image(page.images[0], page.title);
+          }
+
+        } else if (nodes[title]) {
+          // Already here, but has no data - missing wikipedia page, mark as such.
+          nodes[title].links_queried = true;
+          nodes[title].element.css({"color": "#AAA", "text-decoration": "line-through"}).find(".wiki").remove();
+        } else {
+          $("<p />").text("No match for '" + title + "'.").appendTo('#results');
+        }
+    }
+  ); else result = false;
+  return result;
+}
+
+
+function get_image(image_title, article_title) {
+  $.getJSON(WIKI_API_URL,
+    {
+      "action": "query",
+      "prop": "imageinfo",
+      "iiprop": "url|size",
+      "iiurlwidth": MAX_IMG_WIDTH, // @todo remove magics
+      "iiurlheight": MAX_IMG_HEIGHT,
+      "redirects": "1",
+      "format": "json",
+      "titles": "File:" + image_title,
+    },
+    function(data) {
+      $.each(data.query.pages, function(page_id, page_data) {
+        var image_data = page_data.imageinfo[0];
+        if (image_data.width > MAX_IMG_WIDTH || image_data.height > MAX_IMG_HEIGHT) {
+          var url = image_data.thumburl;
+        } else {
+          var url = image_data.url;
+        }
+        var img = $("<img />").attr({"src": url, "title": image_title}).prependTo(nodes[article_title].element.find("div.content"));
+        if (image_data.width > image_data.height && image_data.width > MAX_IMG_WIDTH) {
+            ratio = MAX_IMG_WIDTH / data.image_data;
+            img.css({"width": MAX_IMG_WIDTH, "height": data.image_data * ratio});
+        } else if (image_data.height > image_data.width && image_data.height > MAX_IMG_HEIGHT) {
+            ratio = 80 / image_data.height;
+            img.css({"width": image_data.width * ratio, "height": MAX_IMG_HEIGHT});
+        }
+        return false; // break foreach
+      });
+    }
+  );
+}
+
+
+function get_see_also(title, depth_to_follow) {
+  depth_to_follow--;
+  $.getJSON(WIKI_API_URL,
+    {
+      "action": "parse",
+      "prop": "sections",
+      "disablepp": "1",
+      "redirects": "1",
+      "format": "json",
+      "page": title,
+    },
+    function(data) {
+      var section_index = null;
+      if (data.parse) {
+        $.each(data.parse.sections, function(i, section_data) { 
+          if ("See also" == section_data.line) {
+            section_index = section_data.index;
+            return false; // break foreach
+          }
+        });
+      }
+      if (section_index) {
+        $.getJSON(WIKI_API_URL,
+          {
+            "action": "parse",
+            "prop": "links",
+            "disablepp": "1",
+            "redirects": "1",
+            "format": "json",
+            "page": title,
+            "section": section_index,
+          },
+          function(data) {
+            var count = 0;
+            $.each(data.parse.links, function(i, link_data) { 
+              if (count >= limit) {
+                return false; // break foreach
+              }
+              var name = link_data["*"];
+              var other_namespace = false;
+              for (var index in OTHER_NAMESPACES) {
+                var prefix = OTHER_NAMESPACES[index] + ":";
+                if (prefix == name.slice(0, prefix.length)) {
+                  other_namespace = true;
+                  break;
+                }
+              }
+              if (!other_namespace) {
+                add_node({"title": name}, title);
+
+                if (get_page(name, depth_to_follow, title))
+                  count++;
+              }
+            });
+          }
+        );
+      } else if (nodes[title].links) {
+        // No "See also" links, use links from the first section
+        var count = 0;
+        for (var link_title in nodes[title].links) {
+          if (count >= limit) {
+            break;
+          }
+          if (get_page(link_title, depth_to_follow, title))
+            count++;
+        }
+      } else {
+        // No "See also" links and no links in the first section, get categories at least
+        get_categories(title, depth_to_follow+1);
+      }
+    }
+  );
+}
+
+
+function get_categories(title, depth_to_follow) {
+  depth_to_follow--;
+  $.getJSON(WIKI_API_URL,
+    {
+      "action": "query",
+      "prop": "categories",
+      "clshow": "!hidden",
+      "cllimit": limit,
+      "redirects": "1",
+      "format": "json",
+      "titles": title,
+    },
+    function(data) {
+      $.each(data.query.pages, function(page_id, page_data) { 
+        var count = 0;
+        $.each(page_data.categories, function(i, category_data) {
+          if (count >= limit) {
+            return false; // break foreach
+          }
+          add_node({"title": category_data.title}, title);
+
+          if (get_page(category_data.title, depth_to_follow, title))
+            count++;
+        });
+        return false; // break foreach
+      });
+    }
+  );
+}
